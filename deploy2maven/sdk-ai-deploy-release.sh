5613bef69e5dda919bf0a61d544545fc04e02d59
Tinet-huwk <huwk@ti-net.com.cn>
2019-10-16 15:11:06 +0800

    submit

--- deploy2maven/sdk-ai-deploy-release.sh
+++ deploy2maven/sdk-ai-deploy-release.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cd ../smartlink-sdk-java-ai
 mvn deploy -DaltDeploymentRepository=local::default::http://172.16.203.9:8081/nexus/content/repositories/releases/ -DskipTests
 mvn deploy -DaltDeploymentRepository=aws::default::http://dev.clink.cn:8081/repository/maven-releases/ -DskipTests
