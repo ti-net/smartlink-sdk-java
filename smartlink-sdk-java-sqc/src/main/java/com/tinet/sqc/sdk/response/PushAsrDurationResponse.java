9a645b9b5051778f3759142aa570f60425ba99bc
Tinet-liuhy <liuhy@ti-net.com.cn>
2019-10-24 09:57:56 +0800

    BigBoss对接慧智 asr转写 日流量推送 统计asr的每日每个用户的日流量推送至openApi

---
+++ smartlink-sdk-java-sqc/src/main/java/com/tinet/sqc/sdk/response/PushAsrDurationResponse.java
@@ -0,0 +1,19 @@
+package com.tinet.sqc.sdk.response;
+
+import com.tinet.smartlink.sdk.core.response.BaseResponse;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * BigBoss对接慧智 推送asr转写结果 统计时长的响应内容
+ *
+ * @author liuhy
+ * @date 2019/10/23
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class PushAsrDurationResponse extends BaseResponse {
+
+    private String result;
+    private String message;
+}
