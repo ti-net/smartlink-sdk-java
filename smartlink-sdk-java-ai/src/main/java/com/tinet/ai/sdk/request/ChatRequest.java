ac93b2dbf4dace781fc898a5c576cf32a948da00
zhaojy <zhaojy@ti-net.com.cn>
2019-10-29 15:47:37 +0800

    机器人sdk提交

--- smartlink-sdk-java-ai/src/main/java/com/tinet/ai/sdk/request/ChatRequest.java
+++ smartlink-sdk-java-ai/src/main/java/com/tinet/ai/sdk/request/ChatRequest.java
@@ -10,8 +10,28 @@ import lombok.Data;
  */
 @Data
 public class ChatRequest {
+
+	/**
+	 * 会话唯一标识
+	 */
 	private String uniqueId;
+	/**
+	 * 对话类型(此字段供CC的asr转写结果使用)
+	 * intermediateResult  中间结果
+	 * sentenceEnd 一个完整的句子
+	 */
+	private String type;
+	/**
+	 * 用户说的话
+	 */
 	private String query;
+	/**
+	 * 播放状态
+	 */
 	private Boolean playStatus;
+	/**
+	 * 意图
+	 */
 	private String intent;
+
 }
