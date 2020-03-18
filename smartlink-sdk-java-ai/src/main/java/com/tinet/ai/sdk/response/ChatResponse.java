74314c08a2d5035ce13d1736f4484325c78e783b
zhaojy <zhaojy@ti-net.com.cn>
2020-03-18 19:47:21 +0800

    ai-sdk,增加扩展

--- smartlink-sdk-java-ai/src/main/java/com/tinet/ai/sdk/response/ChatResponse.java
+++ smartlink-sdk-java-ai/src/main/java/com/tinet/ai/sdk/response/ChatResponse.java
@@ -14,7 +14,10 @@ import java.util.List;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ChatResponse {
-
+	/**
+	 * 企业Id
+	 */
+	private Integer userId;
 	/**
 	 * Cdr 中的 uniqueId
 	 */
@@ -41,12 +44,15 @@ public class ChatResponse {
 	private Boolean playInterrupt;
 
 	/**
-	 * 播放类型：tts、voice、url
+	 * 播放类型：tts、voice、url、extends
 	 */
 	private String playType;
 
 	/**
 	 * 播放对象，针对上述播放类型，分别是：TTS文本、录音文件ID、录音文件URL地址
+	 * extends:播放多个语音
+	 * [{"playType":"tts","playObject":"你好"},{"playType":"voice","playObject":"3123123/23131"}]
+	 * [{"playType":"tts", "playObject":"您好您是","playType":"variables", "playObject":"${name}","playType":"tts","playObject":"吗？"}]
 	 */
 	private String playObject;
 
@@ -54,11 +60,6 @@ public class ChatResponse {
 	 * 动作指令，可能会有多个，按顺序执行，例如先挂机（END），再发送短信（SMS）。动作指令统一在播放（play）完成后执行。
 	 */
 	private List<String> action;
-
-	/**
-	 * 企业Id
-	 */
-	private Integer userId;
 
 	/**
 	 * 机器人响应文本
