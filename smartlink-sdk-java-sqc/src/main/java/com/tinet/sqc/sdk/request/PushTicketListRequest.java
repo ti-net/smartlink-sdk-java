127c6f89591079767213c5ca9d1fde2e4f7062b1
chenjg@email.com <1584085282@qq.com>
2020-12-16 18:46:12 +0800

    工单request添加工单声音等

--- smartlink-sdk-java-sqc/src/main/java/com/tinet/sqc/sdk/request/PushTicketListRequest.java
+++ smartlink-sdk-java-sqc/src/main/java/com/tinet/sqc/sdk/request/PushTicketListRequest.java
@@ -3,6 +3,7 @@ package com.tinet.sqc.sdk.request;
 
 import com.tinet.smartlink.sdk.core.HttpMethodType;
 import com.tinet.smartlink.sdk.core.request.BaseRequest;
+import com.tinet.sqc.sdk.model.OriginData;
 import com.tinet.sqc.sdk.model.TicketRecord;
 import com.tinet.sqc.sdk.response.PushTicketListResponse;
 
@@ -39,10 +40,54 @@ public class PushTicketListRequest extends BaseRequest<PushTicketListResponse> {
      * 平台推送的工单id
      */
     private String ticketId;
+
+    /**
+     * 工单结束时间(时间戳，单位：s)
+     */
+    private Long ticketFinishedTime;
+    /**
+     * 工单1级声音类型
+     */
+    private String ticketVoiceType1;
+
+    /**
+     * 工单2级声音类型
+     */
+    private String ticketVoiceType2;
+
+    /**
+     * 工单3级声音类型
+     */
+    private String ticketVoiceType3;
+
+    /**
+     * 工单4级声音类型(可能为空)
+     */
+    private String ticketVoiceType4;
+
+
+
     /**
      * 工单节点内容
      */
     private List<TicketRecord> ticketRecord;
+
+    /**
+     * 自定义字段传值list
+     */
+    private List<OriginData> originDataList;
+
+
+    public List<OriginData> getOriginDataList() {
+        return originDataList;
+    }
+
+    public void setOriginDataList(List<OriginData> originDataList) {
+        this.originDataList = originDataList;
+        if (originDataList != null) {
+            putBodyParameter("originDataList", originDataList);
+        }
+    }
 
     public String getUserId() {
         return userId;
@@ -107,6 +152,61 @@ public class PushTicketListRequest extends BaseRequest<PushTicketListResponse> {
         this.ticketId = ticketId;
         if (ticketId != null) {
             putBodyParameter("ticketId", ticketId);
+        }
+    }
+
+    public Long getTicketFinishedTime() {
+        return ticketFinishedTime;
+    }
+
+    public void setTicketFinishedTime(Long ticketFinishedTime) {
+        this.ticketFinishedTime = ticketFinishedTime;
+        if(ticketFinishedTime != null ){
+            putBodyParameter("ticketFinishedTime", ticketFinishedTime);
+        }
+    }
+
+    public String getTicketVoiceType1() {
+        return ticketVoiceType1;
+    }
+
+    public void setTicketVoiceType1(String ticketVoiceType1) {
+        this.ticketVoiceType1 = ticketVoiceType1;
+        if(ticketVoiceType1 != null ){
+            putBodyParameter("ticketVoiceType1", ticketVoiceType1);
+        }
+    }
+
+    public String getTicketVoiceType2() {
+        return ticketVoiceType2;
+    }
+
+    public void setTicketVoiceType2(String ticketVoiceType2) {
+        this.ticketVoiceType2 = ticketVoiceType2;
+        if(ticketVoiceType2 != null ){
+            putBodyParameter("ticketVoiceType2", ticketVoiceType2);
+        }
+    }
+
+    public String getTicketVoiceType3() {
+        return ticketVoiceType3;
+    }
+
+    public void setTicketVoiceType3(String ticketVoiceType3) {
+        this.ticketVoiceType3 = ticketVoiceType3;
+        if(ticketVoiceType3 != null ){
+            putBodyParameter("ticketVoiceType3", ticketVoiceType3);
+        }
+    }
+
+    public String getTicketVoiceType4() {
+        return ticketVoiceType4;
+    }
+
+    public void setTicketVoiceType4(String ticketVoiceType4) {
+        this.ticketVoiceType4 = ticketVoiceType4;
+        if(ticketVoiceType4 != null ){
+            putBodyParameter("ticketVoiceType4", ticketVoiceType4);
         }
     }
 
