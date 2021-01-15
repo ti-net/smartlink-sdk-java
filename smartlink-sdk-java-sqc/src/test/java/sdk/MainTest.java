407a7f76db629884803fea7e8d4866c12f2bc5fa
liyl <liyl@ti-net.com.cn>
2021-01-15 16:11:18 +0800

    测试代码

--- smartlink-sdk-java-sqc/src/test/java/sdk/MainTest.java
+++ smartlink-sdk-java-sqc/src/test/java/sdk/MainTest.java
@@ -47,4 +47,43 @@ public class MainTest {
         ListCdrsByConditionsResponse responseModel = smartlinkClient.getResponseModel(listCdrsRequest);
         System.out.println(responseModel);
     }
+
+    @Test
+    public void cdrsList () {
+//        System.setProperty("proxyHost", "10.1.27.102");
+//        System.setProperty("proxyPort", "8080");
+//        System.setProperty("proxySet", "true");
+        List<String> uniqueIdList = new ArrayList<>();
+        //uniqueIdList.add("2c3de133d6214d049d3239e6d46bf507-1576739000.7035");
+        uniqueIdList.add("110YY-1587873248.1");
+//        uniqueIdList.add("aknabab-131.24");
+
+        SmartlinkClientConfiguration configuration = new SmartlinkClientConfiguration();
+        // 这些是必须设置的参数，慧智提供
+        configuration.setAccessKeyId("397548UC977E9KMP33D7");
+        configuration.setAccessKeySecret("w5j99jfyqm5e2x8p786ra48qu839j3zj");
+        configuration.setHost(new HttpHost("smartlink-sqc-openapi-test.tinetcloud.com"));
+        // configuration.set...  设置其他参数(超时时间....)
+        //configuration.setProxy(new HttpHost("10.1.27.102", 8080));
+
+        SmartlinkClient smartlinkClient = new SmartlinkClient(configuration);
+
+        ListCdrsByConditionsRequest listCdrsRequest = new ListCdrsByConditionsRequest();
+        listCdrsRequest.setUniqueIdList(uniqueIdList);
+        listCdrsRequest.setStartChannelTime("2020-04-23 00:00:00");
+        listCdrsRequest.setEndChannelTime("2020-04-30 23:59:59");
+
+        ListCdrsByConditionsResponse responseModel = null;
+        try {
+            responseModel =smartlinkClient.getResponseModel(listCdrsRequest);
+            System.out.println(responseModel.toString());
+        } catch (ServerException e) {
+            // 服务器错误
+            e.printStackTrace();
+        } catch (ClientException e) {
+            // 客户端错误
+            e.printStackTrace();
+            e.getMessage();
+        }
+    }
 }
