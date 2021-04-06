6dbb48ffaaab32bf112b81476be7ca1f40254f4f
Tinet-huwk <huwk@ti-net.com.cn>
2021-04-06 13:32:25 +0800

    1.1.0.8-release 提交

--- smartlink-sdk-java-ai/src/main/java/com/tinet/ai/sdk/handler/TibotSessionHandler.java
+++ smartlink-sdk-java-ai/src/main/java/com/tinet/ai/sdk/handler/TibotSessionHandler.java
@@ -2,11 +2,16 @@ package com.tinet.ai.sdk.handler;
 
 
 import com.tinet.ai.sdk.BotWebSocketClient;
+import com.tinet.ai.sdk.ClientSession;
+import com.tinet.ai.sdk.response.Pong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.stomp.*;
 
+import java.lang.reflect.Type;
 import java.net.ConnectException;
+import java.util.Map;
 
 /**
  * 订阅接收服务端通过WebSocket发送的信息，转交给  {@code TibotEventHandler} 处理，各平台处理
@@ -38,6 +43,48 @@ public class TibotSessionHandler extends StompSessionHandlerAdapter {
     @Override
     public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
         logger.info("connect TBot server successful  ~(￣▽￣)~");
+
+        subscribePong();
+
+        Map<String, ClientSession> clientSessionMap = tbotWebSocketClient.getClientSessionMap();
+        if (!clientSessionMap.isEmpty()) {
+            // 重连成功
+            AfterConnectHandler afterConnect;
+            if ((afterConnect = tbotWebSocketClient.getAfterConnect()) != null) {
+                // 用户自己处理session
+                logger.info("[TBot] reConnected, handler old session... ");
+                afterConnect.handlerClientSessionAfterConnect(clientSessionMap);
+                clientSessionMap.clear();
+            } else {
+                // 客户端自动进行重新订阅
+                for (Map.Entry<String, ClientSession> sessionEntry : clientSessionMap.entrySet()) {
+                    logger.info("[TBot] reConnected, login, current loginId :{}", sessionEntry.getKey());
+                    tbotWebSocketClient.login(sessionEntry.getValue());
+                }
+            }
+        }
+    }
+
+    private void subscribePong() {
+        StompHeaders headers = new StompHeaders();
+        headers.setDestination("/chat/pong/" + BotWebSocketClient.CLIENT_UUID);
+        tbotWebSocketClient.getSession().subscribe(headers,
+                new StompFrameHandler() {
+                    @Override
+                    @NonNull
+                    public Type getPayloadType(@NonNull StompHeaders headers) {
+                        return Pong.class;
+                    }
+
+                    @Override
+                    public void handleFrame(@NonNull StompHeaders headers, Object pong) {
+                        if (pong instanceof Pong) {
+                            BotWebSocketClient.unConnectCount.set(0);
+                            logger.info("[TBot] received pong  HOST_UUID:{}", ((Pong) pong).getRequestId());
+                        }
+                    }
+                }
+        );
     }
 
     @Override
