17de3b2bf2818b67a51f733cc0bd350ae27f05ad
liurf <liurf@ti-net.com.cn>
2021-10-12 15:45:12 +0800

    德邦 理赔话单 花旦接口改造

--- smartlink-sdk-java-sqc/src/test/java/sdk/TestClient.java
+++ smartlink-sdk-java-sqc/src/test/java/sdk/TestClient.java
@@ -8,10 +8,12 @@ import com.tinet.smartlink.sdk.core.exceptions.ServerException;
 import com.tinet.sqc.sdk.model.PlatformEnum;
 import com.tinet.sqc.sdk.model.ProductEnum;
 import com.tinet.sqc.sdk.model.Record;
+import com.tinet.sqc.sdk.request.CdrSqcDetailsRequest;
 import com.tinet.sqc.sdk.request.EnableBiFunctionRequest;
 import com.tinet.sqc.sdk.request.JumpUserCheckRequest;
 import com.tinet.sqc.sdk.request.PermissionRequest;
 import com.tinet.sqc.sdk.request.PushCdrRequest;
+import com.tinet.sqc.sdk.response.CdrSqcDetailsResponse;
 import com.tinet.sqc.sdk.response.EnableBiFunctionResponse;
 import com.tinet.sqc.sdk.response.JumpUserCheckResponse;
 import com.tinet.sqc.sdk.response.PermissionResponse;
@@ -33,21 +35,104 @@ import java.util.List;
 public class TestClient {
     SmartlinkClient authClient;
 
+
     @Before
     public void init() {
         SmartlinkClientConfiguration configuration = new SmartlinkClientConfiguration();
+//        configuration.setAccessKeyId("DA5D86BM41F6LH31FN4L");
+//        configuration.setAccessKeySecret("60prd9176j956iv680113az66243mk61");
+//        configuration.setHost("smartlink-sqc-openapi.tinetcloud.com");
+//        configuration.setHost("localhost",8083);
 
-//        configuration.setAccessKeyId("U550M65OOC1Y7842Y985");
-//        configuration.setAccessKeySecret("58w82m7j0bop2g4g5ghaprh43076p951");
-        configuration.setAccessKeyId("59F6WZYJ6PT4G879D318");
-        configuration.setAccessKeySecret("u610p9q44llvd1c0qluj06yq3d1946kz");
-        //configuration.setHost("localhost", 8083);
-        configuration.setHost("smartlink-sqc-openapi.tinetcloud.com");
+//        configuration.setAccessKeyId("59F6WZYJ6PT4G879D318");
+//        configuration.setAccessKeySecret("u610p9q44llvd1c0qluj06yq3d1946kz");
+//        configuration.setHost("smartlink-sqc-openapi-test2.tinetcloud.com");
+
+        configuration.setAccessKeyId("U550M65OOC1Y7842Y985");
+        configuration.setAccessKeySecret("58w82m7j0bop2g4g5ghaprh43076p951");
+//        configuration.setAccessKeyId("59F6WZYJ6PT4G879D318");
+//        configuration.setAccessKeySecret("u610p9q44llvd1c0qluj06yq3d1946kz");
+//        //configuration.setHost("localhost", 8083);
+//        configuration.setHost("smartlink-sqc-openapi-test.tinetcloud.com");
+        configuration.setHost("smartlink-sqc-openapi-test.tinetcloud.com");
+//        configuration.setHost("smartlink-openapi.clink.cn");
+//        //configuration.setHost(new HttpHost("smartlink-openapi.clink.cn",443,"https"));
+
+
         configuration.setConnectionRequestTimeout(100000);
         configuration.setConnectTimeout(100000);
         configuration.setSocketTimeout(100000);
         authClient = new SmartlinkClient(configuration);
     }
+
+
+
+
+    @Test
+    public void getDetail() throws ServerException, ClientException {
+        CdrSqcDetailsRequest request=new CdrSqcDetailsRequest();
+        request.setUniqueId("medias_1-1632464196.22");
+        request.setUserId("8000559");
+
+        CdrSqcDetailsResponse responseModel = authClient.getResponseModel(request);
+        System.out.println(responseModel.toString());
+    }
+
+
+
+    @Test
+    public void pushCdr1() throws ServerException, ClientException, JsonProcessingException {
+        PushCdrRequest pushCdrRequest = new PushCdrRequest();
+        pushCdrRequest.setUserId("110Y");
+        String uniqueid = "medias_1-1634004755.22";
+        pushCdrRequest.setUniqueId(uniqueid);
+        pushCdrRequest.setMainUniqueId(uniqueid);
+        pushCdrRequest.setCdrType("cdr_ib");
+        pushCdrRequest.setCallType(1);
+        pushCdrRequest.setStartTime(1634004708L);
+        pushCdrRequest.setAnswerTime(1634004708L);
+        pushCdrRequest.setEndTime(1634004708L);
+        pushCdrRequest.setBridgeTime(1634004708L);
+        pushCdrRequest.setBridgeDuration(23);
+        pushCdrRequest.setTotalDuration(26);
+        pushCdrRequest.setStatus(1);
+        pushCdrRequest.setEndReason(1);
+        pushCdrRequest.setCno("2021");
+        pushCdrRequest.setAgentName("yinzk123456789");
+        pushCdrRequest.setCustomerNumber("18832015829");
+        pushCdrRequest.setCustomerNumberType("2");
+        pushCdrRequest.setCustomerAreaCode("0310");
+        pushCdrRequest.setCustomerProvince("河北");
+        pushCdrRequest.setCustomerCity("邯郸");
+        pushCdrRequest.setJoinQueueTime(1634004708L);
+        pushCdrRequest.setRecordFile("8000559-20210924141636-01012345678-221221--record-medias_1-1633938133.13");
+        pushCdrRequest.setNumberTrunk("02759566331");
+        pushCdrRequest.setHotline("02759566331");
+//        pushCdrRequest.setCallbackUrl("http://api-bj-test5.clink.cn/internal/sqc/asr?platform=SMARTLINK&Timestamp=1632464243&AccessKeyId=b22f93a2d70b6c8125c94c20eb488027");
+        pushCdrRequest.setQno("1211");
+        pushCdrRequest.setQname("yinzk-test");
+        pushCdrRequest.setFileAuthFlag(true);
+
+//
+//        pushCdrRequest.setBusinessPriority("1");
+        Record recordAgent = new Record();
+//        String fileUrlAgent = "https://yin-file.oss-cn-beijing.aliyuncs.com/smartlink-demo/20200820194226-1890212-1885214-1597923745.146874-in.wav?Expires=1599226249&OSSAccessKeyId=TMP.3Kj7jy8W64BU9ZAhVBma1RRcBHLqkXdmqsYc4YtyGCmDuiPEz8oVm3jZPC1LYzNKZrE8qvu3i7tJCg8ZRxFMFpXFsNXVh2&Signature=L7IUPjkfRCOQg1X%2BRm4o5lPDCeo%3D";
+//        String fileUrlClient = "https://yin-file.oss-cn-beijing.aliyuncs.com/smartlink-demo/20200820194226-1890212-1885214-1597923745.146874-out.wav?Expires=1599226273&OSSAccessKeyId=TMP.3Kj7jy8W64BU9ZAhVBma1RRcBHLqkXdmqsYc4YtyGCmDuiPEz8oVm3jZPC1LYzNKZrE8qvu3i7tJCg8ZRxFMFpXFsNXVh2&Signature=IzLCVzpuU5h4Ev7EPZ1hlCwrGcc%3D";
+        recordAgent.setFileId("path.amVGaWxlU3lzdGVtL00wMC8wRS8xMi93S2dBMUdGZ0Z5MkFGZ0wxQUFCLUFEbUluTTQ3NjMubXAz");
+        recordAgent.setErpetualUrl(true);
+//        recordAgent.setRecordSide("agent");
+//        Record recordClient = new Record();
+//        recordClient.setFileUrl(fileUrlClient);
+//        recordClient.setRecordSide("client");
+        List<Record> list = new ArrayList<>();
+        list.add(recordAgent);
+//        list.add(recordClient);
+        pushCdrRequest.setRecords(list);
+        //继续添加等话单信息
+        PushCdrResponse responseModel = authClient.getResponseModel(pushCdrRequest);
+        System.out.println(responseModel.toString());
+    }
+
 
 
     @Test
